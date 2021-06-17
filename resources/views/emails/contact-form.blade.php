f496aefdb472481c3ecc7150df075f846d025147
Carlos Henrique <chsjr1996@gmail.com>
2021-06-17 00:45:45 -0300

    Little layout changes on contact-form mail template

--- resources/views/emails/contact-form.blade.php
+++ resources/views/emails/contact-form.blade.php
@@ -1,14 +1,21 @@
 <table>
-    <tr>
-        <td>Name: {{ $name }}</td>
+    <tr style="background-color:#333333;color:white;font-weight:bold;text-align:center;">
+        <td colspan="2">CONTACTAR</td>
     </tr>
     <tr>
-        <td>Email: {{ $email }}</td>
+        <td style="font-weight:bold;">Name</td>
+        <td>{{ $name }}</td>
     </tr>
     <tr>
-        <td>Phone: {{ $phone }}</td>
+        <td style="font-weight:bold;">Email</td>
+        <td>{{ $email }}</td>
     </tr>
     <tr>
-        <td>Message: {{ $content }}</td>
+        <td style="font-weight:bold;">Phone</td>
+        <td>{{ $phone }}</td>
+    </tr>
+    <tr>
+        <td style="font-weight:bold;">Message</td>
+        <td>{{ $content }}</td>
     </tr>
 </table>
